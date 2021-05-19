1d77a09c9d800fa7afb0f1d4fc7dedaa78c2ecb5
mohamed7632 <36171712+mohamed7632@users.noreply.github.com>
2021-05-19 23:36:31 +0200

    add job views & urls

--- job/models.py
+++ job/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 JOB_TYPE=(
     ('full time','full time'),
     ('part time','part time')
@@ -13,7 +14,12 @@ class Job(models.Model):
       vacancy=models.IntegerField(default=1)
       salary=models.IntegerField(default=0)
       experience=models.IntegerField(default=1)
+      category=models.ForeignKey('Category',on_delete=models.CASCADE)
 
       def __str__(self):
           return self.title
-   
+
+class Category(models.Model):
+    name=models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
