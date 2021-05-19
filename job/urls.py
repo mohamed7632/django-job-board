da3809eade76d5caa53a772f8cf305b425deb4b3
mohamed7632 <36171712+mohamed7632@users.noreply.github.com>
2021-05-20 00:42:18 +0200

    updating  job views and templates

--- job/urls.py
+++ job/urls.py
@@ -2,6 +2,6 @@
 from django.urls import include,path
 from . import views
 urlpatterns = [
-    path('/',views.job_list ),
+    path('',views.job_list),
     path('<int:id>',views.job_detail),
 ]
--- job/views.py
+++ job/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
 def job_list(request):
-    pass
+    job_list=Job.objects.all()
+    context={'jobs':job_list}
+    return render(request,'Job/job_list.html',context)
 
 
 def job_detail(request,id):
-    pass
+    job=Job.objects.get(id=id)
+    context={'job':job}
+    return render(request,'Job/job_details.html',context)
